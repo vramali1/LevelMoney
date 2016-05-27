1b44599911dbc8fc86487705cca22e0239e04913
vramali1 <vramali1@gmail.com>
2016-05-27 15:52:00 -0700

    LevelMoney Display Transaction Summary and Ignore Donut Transaction Summary functionality complete code checkin.
    LevelMoney Display Transaction Summary and Ignore Donut Summary
    functionality complete code checkin.

--- js/transactions.js
+++ js/transactions.js
@@ -6,7 +6,6 @@ function processTransaction(responseObj) {
 	if (transactions.length > 0) {
 		for (i = 0; i < transactions.length; i++) {
 			var transactionTime = transactions[i]['transaction-time'];
-			// if (transactionTime) {
 			transactionTime = new Date(transactionTime);
 			var transactionDate = transactionTime.getFullYear().toString()
 					+ "-" + (transactionTime.getMonth() + 1).toString();
@@ -22,11 +21,9 @@ function processTransaction(responseObj) {
 				transactionOutput[transactionDate].spent = transactionOutput[transactionDate].spent
 						- transactions[i].amount;
 			}
-			// }
 		}
 		var averageOutput = processAverage(transactionOutput);
-
-		return JSON.stringify(averageOutput);
+		return averageOutput;
 	}
 	return null;
 };
@@ -40,26 +37,18 @@ function processAverage(transactionOutput) {
 		totalIncome = totalIncome + transactionOutput[i]['income'];
 		totalSpent = totalSpent + transactionOutput[i]['spent'];
 		totalMonths = totalMonths + 1;
-		transactionOutput[i]['income'] = "$"
-				+ (transactionOutput[i]['income']).toFixed(2).replace(
-						/(\d)(?=(\d{3})+\.)/g, "$1,");
-		transactionOutput[i]['spent'] = "$"
-				+ transactionOutput[i]['spent'].toFixed(2).replace(
-						/(\d)(?=(\d{3})+\.)/g, "$1,");
+		transactionOutput[i]['income'] = processAmount(transactionOutput[i]['income']);
+		transactionOutput[i]['spent'] = processAmount(transactionOutput[i]['spent']);
 	}
 	if (totalIncome == 0) {
 		IncomeAverage = "$" + totalIncome.toFixed(2);
 	} else {
-		IncomeAverage = "$"
-				+ (totalIncome / totalMonths).toFixed(2).replace(
-						/(\d)(?=(\d{3})+\.)/g, "$1,");
+		IncomeAverage = processAmount((totalIncome / totalMonths).toFixed(2));
 	}
 	if (totalSpent == 0) {
 		spentAverage = "$" + totalSpent.toFixed(2);
 	} else {
-		spentAverage = "$"
-				+ (totalSpent / totalMonths).toFixed(2).replace(
-						/(\d)(?=(\d{3})+\.)/g, "$1,");
+		spentAverage = processAmount((totalSpent / totalMonths).toFixed(2));
 	}
 	transactionOutput.average = {
 		spent : spentAverage,
@@ -67,20 +56,108 @@ function processAverage(transactionOutput) {
 	};
 	return transactionOutput;
 }
+
+function ignoreDonutTransaction(responseObj){
+	//“Krispy Kreme Donuts” or “DUNKIN #336784”
+	var parsed = JSON.parse(responseObj);
+	var transactions = parsed.transactions;
+	var transactionOutput = {};
+	var transactionTime;
+	if (transactions.length > 0) {
+		for (i = 0; i < transactions.length; i++) {
+			var transactionTime = transactions[i]['transaction-time'];
+			transactionTime = new Date(transactionTime);
+			var transactionDate = transactionTime.getFullYear().toString()
+					+ "-" + (transactionTime.getMonth() + 1).toString();
+			if (!transactionOutput.hasOwnProperty(transactionDate)) {
+				transactionOutput[transactionDate] = {};
+				transactionOutput[transactionDate].income = 0;
+				transactionOutput[transactionDate].spent = 0;
+			}
+			if (transactions[i].amount >= 0) {
+				transactionOutput[transactionDate].income = transactionOutput[transactionDate].income
+						+ transactions[i].amount;
+			} else {
+				if(transactions[i]['merchant'] !== 'Krispy Kreme Donuts' && transactions[i]['merchant'] !== 'DUNKIN #336784'){
+					transactionOutput[transactionDate].spent = transactionOutput[transactionDate].spent - transactions[i].amount;
+				}
+			}
+		}
+		var averageOutput = processAverage(transactionOutput);
+
+		return averageOutput;
+	}
+	return null;
+}
+
+function processAmount(amount){
+	return "$" + (amount/10000).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,");
+}
 function displayTransactionSummary(responseObj) {
 	var transactionOutput = processTransaction(responseObj);
-	var str = eval('(' + jsFriendlyJSONStringify(transactionOutput) + ')');
+	var str = eval('(' + jsFriendlyJSONStringify(JSON.stringify(transactionOutput)) + ')');
+	drawTable(transactionOutput);
 	writeToDom(str);
 }
+
+function displayIgnoreDonutSummary(responseObj) {
+	var transactionOutput = ignoreDonutTransaction(responseObj);
+	var str = eval('(' + jsFriendlyJSONStringify(JSON.stringify(transactionOutput)) + ')');
+	drawIgnoreDonutSummaryTable(transactionOutput);
+	writeToIgnoreDonut(str);
+}
+
 function writeToDom(content) {
-	$("#results").append("<div>" + content + "</div>");
+	$("#transactionSummaryJSON").append("<br><b>Level Money Transaction Summary in JSON</b><br><br>" + content );
+}
+
+function writeToIgnoreDonut(content) {
+	$("#ignoreDonutSummaryJSON").append("<br><b>Level Money Ignore Donuts Transaction Summary in JSON</b><br><br>" + content);
 }
 function jsFriendlyJSONStringify(s) {
 	return JSON.stringify(s, null, 2).replace(/\u2028/g, '\\u2028').replace(
 			/\u2029/g, '\\u2029').replace(/},/g, '},<br>');
 }
+function drawTable(data) {
+	$("#transactionSummary").append("<b>Level Money Transaction Summary</b><br><br>");
+	$("#DataTable").append("<tr><th>Month</th><th>Income</th><th>Spent</th></tr>");
+    for (var i in data) {
+    	if(i !== "average"){
+    		var row = $("<tr />");
+            $("#DataTable").append(row);
+            row.append($("<td>" + i + "</td>"));
+            row.append($("<td>" + data[i].income + "</td>"));
+            row.append($("<td>" + data[i].spent + "</td>"));
+    	}
+    }
+    $("#averageIncome").append("<br>Average Income : <b>" + data.average.income +"</b>");
+    $("#averageExpense").append("Average Spent : <b>" +data.average.spent +"</b>");
+}
+
+
+
+function drawIgnoreDonutSummaryTable(data) {
+	debugger;
+	$("#ignoreDonut").append("<br><b>Level Money Ignore Donuts Transaction Summary</b><br><br>");
+	$("#ignoreDonutTable").append("<tr><th>Month</th><th>Income</th><th>Spent</th></tr>");
+    for (var i in data) {
+    	if(i !== "average"){
+    		var row = $("<tr />");
+            $("#ignoreDonutTable").append(row);
+            row.append($("<td>" + i + "</td>"));
+            row.append($("<td>" + data[i].income + "</td>"));
+            row.append($("<td>" + data[i].spent + "</td>"));
+    	}
+    }
+    $("#averageIgnoreDonutIncome").append("<br>Average Income : <b>" + data.average.income +"</b>");
+    $("#averageIgnoreDonutExpense").append("Average Spent : <b>" +data.average.spent +"</b>");
+}
+
+
+
+
+
 function onReady() {
-	var me = this;
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST",
 			"https://prod-api.level-labs.com/api/v2/core/get-all-transactions",
@@ -89,9 +166,10 @@ function onReady() {
 	xhr.setRequestHeader('Accept', 'application/json');
 	xhr.onloadend = function(response) {
 		displayTransactionSummary(this.response);
+		displayIgnoreDonutSummary(this.response);
 	};
 	xhr.onerror = function(err) {
-		document.getElementById('menuPanel').textContent = "ugh an error. i can't handle this right now.";
+		document.getElementById('menuPanel').textContent = "Level Money Transaction REST API is down and not working. Please try again later";
 	};
 	args = {
 		"args" : {
